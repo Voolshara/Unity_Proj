a78fc2406bfa70387100dfc73c9e36df2146ab22
Stanislav <sta-tun-slav@yandex.ru>
2022-07-07 19:01:31 +0300

    Last version

--- Assets/scripts/main_hero/main.cs
+++ Assets/scripts/main_hero/main.cs
@@ -1,68 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class main : MonoBehaviour
 {    
     public float rotationSpeed = 30f;
     public float deadZoneDegrees = 15f;
-    public float speed = 10.0f;
+
+
+    public float Speed = 5f;
+    [SerializeField] public float JumpForce = 300f;
+    [SerializeField] public float DownForce = 300f;
  
     private Transform cameraT;
     private Vector3 cameraDirection;
     private Vector3 playerDirection;
     private Quaternion targetRotation;
+
+
+    //что бы эта переменная работала добавьте тэг "Ground" на вашу поверхность земли
+    private bool _isGrounded;
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
  
     private void Awake()
     {
         cameraT = Camera.main.transform;
     }
  
-    private void Update()
+    private void FixedUpdate()
     {   
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        transform.Translate(0, 0, deltaZ * Time.deltaTime);
-
-        float deltaX = Input.GetAxis("Horizontal");
-        Vector3 rot_Vector;
-        if(deltaX > 0) rot_Vector = new Vector3(90, 0, 0);
-        else if(deltaX < 0) rot_Vector = new Vector3(-90, 0, 0);
-        else rot_Vector = new Vector3(0, 0, 0);
+        // - Слежение за камерой
 
         cameraDirection = new Vector3(cameraT.forward.x, 0f, cameraT.forward.z);
         playerDirection = new Vector3(transform.forward.x, 0f, transform.forward.z);
 
-        if(Vector3.Angle(cameraDirection + arot_Vector, playerDirection) > deadZoneDegrees)
+        if(Vector3.Angle(cameraDirection, playerDirection) > deadZoneDegrees)
         {
-            targetRotation = Quaternion.LookRotation(cameraDirection + rot_Vector, transform.up);
+            targetRotation = Quaternion.LookRotation(cameraDirection, transform.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime * Mathf.Pow(1.1f, 
                                                                                                             (Vector3.Angle(cameraDirection, playerDirection) / 10)));
         }
 
+        // - Слежение за камерой
+
+        // - Движение
+
+        MovementLogic();
+        JumpLogic();
+        ChechJump();
+        Debug.Log(_isGrounded);
+    }
+
+    private void MovementLogic()
+    {     
+        if (_isGrounded ||  !_isGrounded)
+            {
+                float moveHorizontal = Input.GetAxis("Horizontal");
+
+                float moveVertical = Input.GetAxis("Vertical");
+
+                Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+                // что бы скорость была стабильной в любом случае
+                // и учитывая что мы вызываем из FixedUpdate мы умножаем на fixedDeltaTimе
+                transform.Translate(movement * Speed * Time.fixedDeltaTime);
+            }
+    }
+
+    private void JumpLogic()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (_isGrounded)
+            {
+                // Обратите внимание что я делаю на основе Vector3.up а не на основе transform.up
+                // если наш персонаж это шар -- его up может быть в том числе и вниз и влево и вправо. 
+                // Но нам нужен скачек только вверх! Потому и Vector3.up
+                _rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void ChechJump(){
+
+        if (!_isGrounded){
+
+            _rb.AddForce(-transform.up * DownForce, ForceMode.Impulse);
+
+        }
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        IsGroundedUpate(collision, true);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        IsGroundedUpate(collision, false);
+    }
+
+    private void IsGroundedUpate(Collision collision, bool value)
+    {
+        if (collision.gameObject.tag == ("Ground"))
+        {
+            _isGrounded = value;
+        }
     }
 }
-
-
-
-
-//     public float speedH = 2.0f;
-//     public float speedV = 2.0f;
- 
-//     private float yaw = 0.0f;
-//     private float pitch = 0.0f;
-//     public float maxDegreesDelta = 15f;
- 
- 
-//     private void Start()
-//     {
-       
- 
-//     }
- 
-//     void Update()
-//     {
-        
- 
-//     }
-// }
-// }
